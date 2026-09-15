17920717c785ca6c1f7f0629f5f775a6cc369922
Ronak <ronakyadav1609@gmail.com>
2026-09-15 21:29:33 +0530

    a11y: add keyboard skip link for primary page content

---
+++ src/components/SkipLink.tsx
@@ -0,0 +1,10 @@
+const SkipLink = () => (
+  <a
+    href="#main-content"
+    className="sr-only focus:not-sr-only focus:fixed focus:left-4 focus:top-4 focus:z-[100] focus:bg-white focus:px-4 focus:py-3 focus:text-sm focus:font-bold focus:text-black"
+  >
+    Skip to main content
+  </a>
+);
+
+export default SkipLink;
